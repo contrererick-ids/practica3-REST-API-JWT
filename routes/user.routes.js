1d08847b64a37629b84dd0f2775e8028b157ba25
contrererick-ids <is722185@iteso.mx>
2025-04-28 21:12:57 -0600

    feat(routes): Define user routes with authentication middleware
    >> Created user.routes.js to manage user-related endpoints. Added routes for user creation, login, retrieval, update, and deletion. Secured sensitive routes with authMiddleware to ensure only authenticated users can access them.

---
+++ routes/user.routes.js
@@ -0,0 +1,21 @@
+// Con require('express') se importa el módulo 'express' que es una biblioteca de aplicaciones web para Node.js.
+const express = require('express');
+// Se crea una instancia del enrutador de Express.
+const router = express.Router();
+// Se importa el controlador de usuarios.
+const userController = require('../controllers/user.controller');
+// Se importa el middleware de autenticación.
+const authMiddleware = require('../middleware/auth');
+
+// Se generan las rutas para el controlador de usuarios.
+router.post('/', userController.createUser);
+router.post('/login', userController.loginUser);
+router.get('/', authMiddleware, userController.getUsers);
+
+// Se generan las rutas para el controlador de usuarios por ID.
+router.get('/:id', authMiddleware, userController.getUserById);
+router.put('/:id', authMiddleware, userController.updateUser);
+router.delete('/:id', authMiddleware, userController.deleteUser);
+
+// Se exporta el enrutador.
+module.exports = router;
